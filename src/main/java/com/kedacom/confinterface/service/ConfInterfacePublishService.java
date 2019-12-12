15ed364f0bce4f1f9dce2e1259cfb907956bdc0d
linlifen <linlifen@kedacom.com>
2019-12-12 09:47:31 +0800

    补充提交：修改发布服务得实现

---
+++ src/main/java/com/kedacom/confinterface/service/ConfInterfacePublishService.java
@@ -0,0 +1,11 @@
+package com.kedacom.confinterface.service;
+
+import com.kedacom.confinterface.dto.MediaResource;
+
+import java.util.List;
+
+public abstract class ConfInterfacePublishService {
+    public abstract void addSubscribeMessage(int type, String groupId, String url);
+    public abstract void publishStatus(String account, String groupId, int status, List<MediaResource> forwardResources, List<MediaResource> reverseResources);
+    public abstract void publishStatus(String account, String groupId, int status);
+}
