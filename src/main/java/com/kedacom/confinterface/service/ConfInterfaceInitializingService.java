7bc87b26b920d8bc19a5c5ce2f35ad22bb2ce9f5
wangchao_jkcpx <wangchao_jkcpx@kedacom.com>
2020-03-18 15:16:40 +0800

    修复ptz控制状态为停止

--- src/main/java/com/kedacom/confinterface/service/ConfInterfaceInitializingService.java
+++ src/main/java/com/kedacom/confinterface/service/ConfInterfaceInitializingService.java
@@ -124,6 +124,8 @@ public class ConfInterfaceInitializingService implements CommandLineRunner {
 
         //启动终端注册Gk
         terminalManageService.StartUp();
+        LogTools.info(LogOutputTypeEnum.LOG_OUTPUT_TYPE_FILE, "no groups in db, init OK! current time : " + System.currentTimeMillis());
+        System.out.println("no groups in db, init OK! current time : " + System.currentTimeMillis());
     }
 
     private void createConferenceManage() {
